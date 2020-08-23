0a0238578e23dc92f148db8f49d955bc048a3db4
Emil Mikulic <emikulic@gmail.com>
2020-08-23 17:22:19 +1000

    Add benchmark of "squares" prng.

--- src/random_benchmark.cc
+++ src/random_benchmark.cc
@@ -5,6 +5,19 @@
 
 namespace {
 
+// Squares: A Fast Counter-Based RNG
+// https://arxiv.org/abs/2004.06278
+uint32_t squares(uint64_t ctr, uint64_t key) {
+  uint64_t x, y, z;
+  y = x = ctr * key;
+  z = y + key;
+  x = x * x + y;
+  x = (x >> 32) | (x << 32); /* round 1 */
+  x = x * x + z;
+  x = (x >> 32) | (x << 32); /* round 2 */
+  return (x * x + y) >> 32;
+}
+
 void BM_Construct(benchmark::State& state) {
   for (auto _ : state) {
     benchmark::DoNotOptimize(Random());
@@ -36,6 +49,15 @@ void BM_Fork(benchmark::State& state) {
   }
 }
 BENCHMARK(BM_Fork);
+
+void BM_Squares(benchmark::State& state) {
+  uint64_t key = 0x1234567812345678;
+  uint64_t ctr = 0;
+  for (auto _ : state) {
+    benchmark::DoNotOptimize(squares(ctr++, key));
+  }
+}
+BENCHMARK(BM_Squares);
 
 }  // namespace
 
